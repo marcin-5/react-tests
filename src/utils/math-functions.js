b02dae8d214a7a3a5d0b544c8165c3b64dfce45c
Marcin Bojara <marcin_5@mail.ru>
2023-11-08 18:43:13 +0100

    add missing export

--- src/utils/math-functions.js
+++ src/utils/math-functions.js
@@ -1,13 +1,13 @@
-function sum(a = 0, b = 0) {
+export function sum(a = 0, b = 0) {
   return a + b;
 }
-function substract(a = 0, b = 0) {
+export function substract(a = 0, b = 0) {
   return a - b;
 }
-function multiply(a = 0, b = 0) {
+export function multiply(a = 0, b = 0) {
   return Number.parseFloat((a * b).toFixed(2));
 }
-function divide(a = 0, b = 0) {
+export function divide(a = 0, b = 0) {
   if (b !== 0) return Number.parseFloat((a / b).toFixed(2));
   throw new Error("You can't divide by 0");
 }
